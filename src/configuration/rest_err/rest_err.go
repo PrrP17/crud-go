b6ec7bbc91b8389a3935b77f71c24dfb533dfd0e
Paulo Roberto <pauloprrp06@gmail.com>
2023-11-07 15:12:10 -0300

    criando o tratamento de erros no go

---
+++ src/configuration/rest_err/rest_err.go
@@ -0,0 +1,77 @@
+package rest_err
+
+import "net/http"
+
+type RestErr struct {
+	Message string   `json:"message"`
+	Err     string   `json:"error"`
+	Code    int64    `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+func NewRestErr(message, err string, code int64, couses []Causes) *RestErr {
+
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  couses,
+	}
+}
+
+func NewBadRequestErr(message string) *RestErr {
+
+	return &RestErr{
+		Message: message,
+		Err:     "bad request",
+		Code:    http.StatusBadRequest,
+	}
+
+}
+
+func NewBadRequestValidationErr(message string, couses []Causes) *RestErr {
+
+	return &RestErr{
+		Message: message,
+		Err:     "bad request",
+		Code:    http.StatusBadRequest,
+		Causes:  couses,
+	}
+
+}
+
+func NewInternalServerErr(message string) *RestErr {
+
+	return &RestErr{
+		Message: message,
+		Err:     "internal server error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundErr(message string) *RestErr {
+
+	return &RestErr{
+		Message: message,
+		Err:     "not found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenErr(message string) *RestErr {
+
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
